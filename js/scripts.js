d109ac2e6456b55e15de98485230f7091868109f
estherndichu <itskuijenga@gmail.com>
2021-01-12 01:30:02 +0300

    add price constructors for crust,toppings and crust

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,31 @@ function Pizza(size, crust) {
     this.crust = crust;
     this.toppings = [];
 }
+var sizePrice = {
+    medium: 800,
+    large: 1200,
+    extraLarge: 1400
+};
+var toppingPrice = [{
+    meat: {
+        medium: 100,
+        large: 150,
+        extraLrge:200
+    },
+    sausage: {
+        medium: 150,
+        large: 200,
+        extraLarge: 250
+    },
+    vegies: {
+        medium: 100,
+        large: 150,
+        extralarge: 200
+    }
+}];
+
+var crustPrice = {
+    crispy: 300,
+    stuffed: 200,
+    glutenFree: 300,
+};
