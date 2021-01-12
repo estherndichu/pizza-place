c2bd4364eb4fbeff009a7a0bfb15228632e89f05
estherndichu <itskuijenga@gmail.com>
2021-01-12 03:03:43 +0300

    add function to calculate price of pizza compositions

--- js/scripts.js
+++ js/scripts.js
@@ -31,3 +31,43 @@ var crustPrice = {
     stuffed: 200,
     glutenFree: 300,
 };
+function calculateSizePrice(size) {
+    if (size === "medium") {
+        return sizePrice.medium * 1;
+    } else if (size === "large") {
+        return sizePrice.large * 1;
+    } else {
+        return sizePrice.extraLarge * 1;
+    }
+};
+
+function calculateCrustPrice(crust) {
+    if (crust === "crispy") {
+        return crustPrice.crispy * 1;
+    } else if (crust === "stuffed") {
+        return crustPrice.stuffed * 1;
+    } else {
+        return crustPrice.glutenFree * 1;
+    }
+}
+
+function calculateToppingsPrice(toppings) {
+    var noOfTopping = 0;
+    for (i = 0; i < toppings.length; i++) {
+        if (toppings[i] == "meat") {
+            noOfTopping += 100;
+        }
+        if (toppings[i] == "sausage") {
+            noOfTopping += 150;
+        }
+        if (toppings[i] == "veggies") {
+            noOfTopping += 100;
+        }
+    }
+    return noOfTopping * 1;
+};
+
+
+function checkPepperoni(topping) {
+    return topping === "pepperoni";
+};
